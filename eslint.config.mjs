263b714e436d47caf6f0a66c14d58525f6929d1b
Gabriel Hernandez <gabrielhernandez@192.168.1.11>
2026-05-13 10:13:53 -0600

    lin nuevo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,17 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-html-link-for-pages": "off",
+      "@next/next/no-assign-module-variable": "off",
+      "prefer-const": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
 ]);
 
 export default eslintConfig;
